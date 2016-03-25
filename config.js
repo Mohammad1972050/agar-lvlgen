0cf22507adb7b8923300bf0a1418dfadaa6da99b
Makan Dey <makan@mastacoder.com>
2016-03-25 15:11:00 -0400

    Add liveConsole to config.js
    The option to turn it on or off!

--- config.js
+++ config.js
@@ -11,10 +11,11 @@ module.exports = {
 	name: "agar-lvlgen", // Name of the bots that will be playing.
 	reset: 0, // Minutes before server reset. (0 for none)
 	showtoken: false, // Show the token of the Facebook account on start.
+	liveConsole: true, // Show a live console instead of new lines.
 
 	// Advance
 	regions: ["BR-Brazil", "CN-China", "EU-London", "JP-Tokyo", "RU-Russia", "SG-Singapore", "TK-Turkey", "US-Atlanta"],
-	statusDelay: 1000, // Delay of milleseconds for console.log.
+	statusDelay: 1000, // Delay of milleseconds for console.log. (1000 recommended for live console)
 	
 	/* Possible values:
 	 *
@@ -22,5 +23,5 @@ module.exports = {
 	 * apos: Beta AI - Better performance, and higher CPU Usage.
 	 * 
 	*/
-	ai: "default"
+	ai: "apos"
 }
